d507d9810f0ec5f5cac179c54af9c955d04d5771
Rostyslav <rdiachuk92@gmail.com>
2021-05-14 20:54:21 +0300

    Initial commit

---
+++ WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public Button btnToMove = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            var circle = new Image();
+            circle.Source =
+                new BitmapImage(
+                    new Uri("C:\\Users\\ROSTIK\\RiderProjects\\WpfApplication2\\WpfApplication2\\index.png", UriKind.Absolute));
+            b0.Content = circle;
+            
+            chessTable.Buttons = new List<Button> {b0, b1, b2, b3, b4, b5, b6, b7, b8};
+        }
+
+        private void Choose(object sender, RoutedEventArgs e)
+        {
+            var senderBtn = ((Button)sender);
+            if (btnToMove == null)
+            {
+                btnToMove = senderBtn;
+                senderBtn.BorderThickness = new Thickness(3);
+            }
+
+            else
+            {
+                if (!Equals(sender, btnToMove) && btnToMove.Content!=null)
+                {
+                    senderBtn.Content = btnToMove.Content;
+                    btnToMove.Content = null;
+                    btnToMove.BorderThickness = new Thickness(1);
+                    btnToMove = null;
+                }
+                else
+                {
+                    btnToMove.BorderThickness = new Thickness(1);
+                    btnToMove = null;  
+                }
+            }
+        }
+    }
+}
---
+++ WpfApplication2/MyTable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace WpfApplication2
+{
+    public class MyTable : Grid
+    {
+        public List<Button> Buttons = new List<Button>();
+
+        public Button this[int i, int j]
+        {
+            get { return Buttons[i * 3 + j - 1]; }
+        }
+    }
+}
